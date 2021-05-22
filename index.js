91cee9114a8b4ab71b0c1a625dcf841cb1110375
Michael Martinez <zeroeclipse@live.com>
2021-05-22 13:08:24 -0400

    commit tree af9e21c683d1f915617bb2c8af95ee926e50b147

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const farmAnimals = 'cow horse sheep pig chicken';
-
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 const muppet = {
@@ -46,3 +45,27 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+const [moo,neigh,baa,oink,cluck] = farmAnimals.split(' ');
+const [bessie,,dolly, babe,little] = farmAnimals.split(' ');
+const [blackAndWhite,, black, pink,] = farmAnimals.split(' ');
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b,, v] = colors;
+const[,,,,,indg] = colors;
+const{muppetName, color,song, job, partner} = muppet
+const{song2, song4} = nestedMuppet.album.theMuppetMovie
+const{nestedJob, nestedPartner} = nestedMuppet;
+// const nestedMuppet = {
+//   nestedName: 'Kermit',
+//   nestedColor: 'green',
+//   album: {
+//     theMuppetMovie: {
+//       song1: 'Rainbow Connection',
+//       song2: 'Moving Right Along',
+//       song3: 'Never Before, Never Again',
+//       song4: 'I Hope That Something Better Comes Along',
+//     },
+//   },
+//   nestedJob: 'Host of The Muppet Show',
+//   nestedPartner: 'Miss Piggy'
+// };
